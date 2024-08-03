391c5f4d4edd5ee9a633c8f65bd68c12963c95f5
Jeremy Theintz <jeremy@appogee.ch>
2024-08-03 13:59:32 +0200

    style: add some containers to the home page

--- src/home/_components/HomePage/HomePage.styles.ts
+++ src/home/_components/HomePage/HomePage.styles.ts
@@ -11,4 +11,16 @@ export const StyledHomePageContainer = styled.div`
 	height: 100dvh;
 `;
 
-export const StyledTitle = styled.h1``;
+export const StyledTitle = styled.h1`
+	margin-top: 80px;
+	margin-bottom: 40px;
+
+	font-size: 2.25rem;
+`;
+
+export const StyledOlympicRingsContainer = styled.div`
+	height: 700px;
+
+	padding-top: 150px;
+	padding-bottom: 150px;
+`;
